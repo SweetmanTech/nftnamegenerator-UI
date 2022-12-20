d2637ba7aa96c3b8003857daddbf787fde135d96
Sweets Sweetman <sweetmantech@gmail.com>
2022-12-20 14:59:05 -0500

    testing without dynamic params.

--- pages/api/mint/index.ts
+++ pages/api/mint/index.ts
@@ -1,14 +1,15 @@
 import axios from "axios"
 
 export default async function handler(req, res) {
-  const { query } = req
-  const {name, description, imageUri, recipient} = query
+  // const { query } = req
+  // const { name, description, imageUri, recipient } = query
   try {
     axios.post(process.env.DEFENDER_AUTOTASK_WEBHOOK, {
-      name: name || "NAME FROM API",
-      description: description || "relay description from POSTMAN",
-      imageUri: imageUri || "ipfs://bafybeidyqy7n2defa767w64g4oj4n63whgfl7mtigwqq6co3i6kg4qlo5u/lilnoun-6473.png",
-      recipient: recipient || "0xcfBf34d385EA2d5Eb947063b67eA226dcDA3DC38"
+      name: "NAME FROM API",
+      description: "relay description from POSTMAN",
+      imageUri:
+        "ipfs://bafybeidyqy7n2defa767w64g4oj4n63whgfl7mtigwqq6co3i6kg4qlo5u/lilnoun-6473.png",
+      recipient:   "0xcfBf34d385EA2d5Eb947063b67eA226dcDA3DC38",
     })
     res.status(200).json({ name: 'John Doe' })
   } catch (err) {
