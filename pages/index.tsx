981252045ac5cbebde69b6f944288bc34b7d9aba
SweetmanTech <SweetmanTech@users.noreply.github.com>
2022-12-20 15:24:27 +0000

    Apply formatting changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,11 @@ import AllowList from "../components/AllowList"
 
 const Home: NextPage = () => (
   <div className={styles.container}>
-    <SeoHead title="nft name generator" description="use AI to inspire your next nft drop" image="" />
+    <SeoHead
+      title="nft name generator"
+      description="use AI to inspire your next nft drop"
+      image=""
+    />
 
     <main className={styles.main}>
       <h1 className={styles.title}>nft name generator</h1>
