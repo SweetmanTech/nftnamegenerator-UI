7b4424b5f73fe2e2892672ce9913dad04bc64246
Sweets Sweetman <sweetmantech@gmail.com>
2022-12-22 11:17:05 -0600

    I change .js => .ts

--- lib/ipfs.ts
+++ lib/ipfs.ts
@@ -5,7 +5,7 @@ const NFT_STORAGE_TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGMzNTA3ZEY4NkNlZTgwZUExNTQyMzhkMDQ4NDVGOTg2MjM0NmI5M0EiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2MzcwMDQ3NTM0MiwibmFtZSI6ImRlY2VudC1zZGsifQ.xKo6Lgznxai4GbZSc5jJwbisjWM9aA-RK7uVifHkrok"
 const client = new NFTStorage({ token: NFT_STORAGE_TOKEN })
 
-export const storeBlob = async (data) => {
+export const storeBlob = async (data: any) => {
   const cid = await client.storeBlob(data)
   return `ipfs://${cid}`
 }
