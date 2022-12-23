64dbdf98cbb0777cf45e3652a72c7a9210472b95
sameer rasa <crazzywizard@gmail.com>
2022-12-23 17:21:57 -0500

    updated component name

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next"
 import SeoHead from "../components/SeoHead"
 import styles from "../styles/Home.module.css"
-import AllowList from "../components/AllowList"
+import NFTNameGenerator from "../components/NFTNameGenerator"
 
 const Home: NextPage = () => (
   <div className="max-w-full bg-cover bg-hero-pattern">
@@ -12,7 +12,7 @@ const Home: NextPage = () => (
     />
 
     <main className={styles.main}>
-      <AllowList />
+      <NFTNameGenerator />
     </main>
   </div>
 )
