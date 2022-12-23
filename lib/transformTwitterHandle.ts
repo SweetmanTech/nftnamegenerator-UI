728e9702acfaaf01a3bb4150d21f8c6eefe8453b
Sweets Sweetman <sweetmantech@gmail.com>
2022-12-23 10:14:20 -0600

    I remove dev logging.

--- lib/transformTwitterHandle.ts
+++ lib/transformTwitterHandle.ts
@@ -1,7 +1,6 @@
 import * as _ from "lodash"
 
 const transformTwitterHandle = (twitterHandle?: string): string => {
-    console.log("twitterHandle", twitterHandle)
     if (twitterHandle) {
       return _.startsWith(twitterHandle, "@", 0) ? twitterHandle : `@${twitterHandle}`
     }
