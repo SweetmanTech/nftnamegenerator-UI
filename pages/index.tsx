631b48f7512a4a8f9995c472bd3a2b66c888795d
sameer rasa <crazzywizard@gmail.com>
2022-12-24 02:23:23 -0500

    css changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next"
 import SeoHead from "../components/SeoHead"
 import styles from "../styles/Home.module.css"
@@ -14,6 +15,22 @@ const Home: NextPage = () => {
         description="use AI to inspire your next nft drop"
         image=""
       />
+      <div className="flex flex-row px-4 space-x-4 bg-white 4 lg:md:bg-transparent m-w-1/2">
+        <a href="https://defient.co" target="_blank" rel="noreferrer">
+          <img
+            src="icons/defient.svg"
+            alt="defient icon"
+            className="object-right-bottom lg:md:object-left-top w-14 h-14"
+          />
+        </a>
+        <a href="https://twitter.com/nftnamegen" target="_blank" rel="noreferrer">
+          <img
+            src="icons/twitter.svg"
+            alt="twitter icon"
+            className="object-right-bottom lg:md:object-left-top w-14 h-14"
+          />
+        </a>
+      </div>
       <main className={styles.main}>
         {!showResults && <NFTNameGenerator />}
         {showResults && <Results />}
