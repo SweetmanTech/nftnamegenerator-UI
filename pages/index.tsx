fa5c2abdab5eab0fd0b5f6b6de07dc5da6aced2c
sameer rasa <crazzywizard@gmail.com>
2022-12-24 01:05:46 -0500

    hidding overflow

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import Results from "../components/Results"
 const Home: NextPage = () => {
   const { showResults } = useNFTNameGenerator()
   return (
-    <div className="max-w-full bg-cover lg:bg-hero-pattern md:bg-hero-pattern bg-mobile-pattern">
+    <div className="max-w-full overflow-y-hidden bg-cover lg:bg-hero-pattern md:bg-hero-pattern bg-mobile-pattern">
       <SeoHead
         title="nft name generator"
         description="use AI to inspire your next nft drop"
