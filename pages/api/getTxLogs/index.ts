56b69877ef4227608c27f99b5c87820fa827469b
sameer rasa <crazzywizard@gmail.com>
2022-12-24 13:16:12 -0500

    fixing opensea issue using alchemy api key

--- pages/api/getTxLogs/index.ts
+++ pages/api/getTxLogs/index.ts
@@ -1,12 +1,14 @@
 /* eslint-disable class-methods-use-this */
-import { EtherscanProvider } from "@ethersproject/providers"
+import { ethers } from "ethers"
 import { createHandler, Get, Query } from "next-api-decorators"
 
 class GetTxLogs {
   @Get()
   async getTxLogs(@Query("txHash") txHash: string) {
-    const network = process.env.NEXT_PUBLIC_CHAIN_ID === "80001" ? "maticmum" : "matic"
-    const provider = new EtherscanProvider(network)
+    const provider = new ethers.providers.AlchemyProvider(
+      Number(process.env.NEXT_PUBLIC_CHAIN_ID),
+      process.env.ALCHEMY_API_KEY,
+    )
     const txLogs = await provider.getTransactionReceipt(txHash)
     if (txLogs) {
       return txLogs
